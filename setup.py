a468792478acc0d29d0a7e1a318f05fa1fec53b4
Simon Fayer <sf105@ic.ac.uk>
2017-10-19 18:03:40 +0100

    Check with python 2.7 and 3.6.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup, find_packages
+
+REQUIRES = ['sqlalchemy']
+REQUIRES2 = ['python-ldap']
+REQURIES3 = ['pyldap']
+
+if sys.version_info[0] < 3:
+  REQUIRES.extend(REQUIRES2)
+else:
+  REQUIRES.extend(REQURIES3)
+
 
 setup(name='examdb',
       version='1.0',
       package_dir={'': 'src'},
       packages=find_packages('src'),
-      install_requires=['python-ldap', 'sqlalchemy'],
+      install_requires=REQUIRES,
      )
 
